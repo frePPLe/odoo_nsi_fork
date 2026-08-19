78ec0c5e47c9881e3190e3a1c90355d58ff82497
hichamlahlou <hlahlou@frepple.com>
2026-08-19 22:32:16 +0200

    remove debug info

--- frepple/controllers/inbound.py
+++ frepple/controllers/inbound.py
@@ -367,9 +367,6 @@ class importer(object):
                                         product=product
                                     )
                                 )
-                                msg.append(
-                                    f"new picking_type is {picking_type.id if picking_type else "null"}"
-                                )
                                 picking_type_id = None
                                 if picking_type:
                                     picking_type_id = picking_type.id
